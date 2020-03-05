971cecb9eded5e2b6dca25b0ac6c50739c04ba30
rdalla <rodvfilho@terra.com.br>
2020-03-04 22:19:41 -0300

    Added a simple control of ACK in the UDP client, but it is not implemented yet on the UDP server.
    Added in the UDP server only a print to show the message that the UDP client
    sent.

--- src/udp_client.py
+++ src/udp_client.py
@@ -1,35 +1,103 @@
-import sys, time
-from socket import *
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-server_port = 12000
+import socket
+import sys
+import os
+import platform
 
-# Receber IP do server como argumento de linha de comando
-argv = sys.argv
-server_ip = argv[1]
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-timeout = 1 # 1 segundo
+#_____________________________________________________________
 
-# Criar socket cliente
-client_socket = socket(AF_INET, SOCK_DGRAM)
-client_socket.settimeout(timeout)
+def ping_IP(UDP_IP_ADDRESS):
+    if platform.system() == "Windows":
+        rep = os.system('ping ' + UDP_IP_ADDRESS +" -n 1")
+    else:
+        rep = os.system('ping -c 3 ' + UDP_IP_ADDRESS)
 
-ping_id = 0
+    while rep != 0:
+        print ("Servidor não encontrado...")
+        UDP_IP_ADDRESS = input('Digite novamente o IP do servidor : ')
+        rep = os.system('ping ' + UDP_IP_ADDRESS)
 
-# ping 10 vezes
-while ping_id < 10:
-    ping_id += 1
+    pass
+#_____________________________________________________________
 
-    try:
+def config():
+    global UDP_IP_ADDRESS
+    global UDP_PORT_NO
+    global MAX_MESSAGE
+    global SEQNO
 
-        rtt_init = time.time()
-        client_socket.sendto("ping".encode(), (server_ip, server_port))
-        message, address = client_socket.recvfrom(1024)
-        rtt_end = time.time()
+    UDP_IP_ADDRESS = raw_input('Digite o IP do servidor : ')
+    UDP_PORT_NO = int(raw_input('Digite a porta desejada entre 10001 e 11000: '))
+    while (UDP_PORT_NO < 10001) or (UDP_PORT_NO > 11000):
+        print("A porta deve ser entre 10001 - 11000")
+        UDP_PORT_NO = int(raw_input('Digite a porta desejada entre 10001 e 11000: '))
 
-        print("Resposta do servidor: " + server_ip + ": " + message.decode() 
-                                        + " RTT: " + str(rtt_end - rtt_init))
-    except:
-        print("Timeout")
-        continue
+    ping_IP(UDP_IP_ADDRESS)
+    print("Servidor Ok!!!")
+    MAX_MESSAGE = int(raw_input('Digite o numero maximo de mensagens: '))
+    SEQNO = 0
 
-client_socket.close()
+    pass
+#_____________________________________________________________
+
+def messenger(UDP_IP_ADDRESS, UDP_PORT_NO, MAX_MESSAGE):
+    Message = 0 #começo as mensagens com zero
+
+    while Message < MAX_MESSAGE:
+
+        if SEQNO == 0:
+            data = str(SEQNO) + str(Message) + str(MAX_MESSAGE)
+            clientSock.sendto(data.encode(), (UDP_IP_ADDRESS, UDP_PORT_NO))
+            print("SENT: ", "SEQNO: " + str(SEQNO) + " / Data: " + str(Message) + " / Maximo: " + str(MAX_MESSAGE))
+            try:
+                clientSock.settimeout(1.0)
+                answer, UDP_IP_ADDRESS = clientSock.recvfrom(1024)
+                answer = answer.decode()
+                ACK = answer
+
+                if ACK == "0":
+                    print("RECV :" + "ACK = " + answer + "\n")
+                    SEQNO = 1
+                    Message = Message + 1
+
+            except timeout:
+                print("RECV: timeout!")
+
+        if SEQNO ==1:
+            data = str(SEQNO) + str(Message) + str(MAX_MESSAGE)
+            clientSock.sendto(data.encode(), (UDP_IP_ADDRESS, UDP_PORT_NO))
+            print("SENT: ", "SEQNO: " + str(SEQNO) + " / Data: " + str(Message) + " / Maximo: " + str(MAX_MESSAGE))
+            try:
+                clientSock.settimeout(1.0)
+                answer, UDP_IP_ADDRESS = clientSock.recvfrom(1024)
+                answer = answer.decode()
+                ACK = answer
+
+                if ACK == "1":
+                    print("RECV :" + "ACK = " + answer + "\n")
+                    SEQNO = 0
+                    Message = Message + 1
+
+            except timeout:
+                print("RECV: timeout!")
+
+    client_socket.close()
+
+
+    pass
+
+#_____________________________________________________________
+
+def main():
+
+    config()
+    messenger(UDP_IP_ADDRESS, UDP_PORT_NO, MAX_MESSAGE)
+
+#_____________________________________________________________
+
+if __name__ == '__main__':
+    main()
--- src/udp_server.py
+++ src/udp_server.py
@@ -1,17 +1,49 @@
-from socket import *
-import random
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-server_socket = socket(AF_INET, SOCK_DGRAM)
-server_socket.bind(('', 12000))
+import socket
+import sys
+import os
+import platform
 
-while True:
-    print("Rodando...")
-    rand = random.randint(0, 10)
+#_____________________________________________________________
 
-    message, address = server_socket.recvfrom(1024)
-    print("Mensagem Recebida: " + message.decode())
+def config():
+    global UDP_IP_ADDRESS
+    global UDP_PORT_NO
 
-    if rand < 4:
-        continue
 
-    server_socket.sendto("pong", address)
+    UDP_IP_ADDRESS = raw_input('Digite o IP do servidor : ')
+    UDP_PORT_NO = int(raw_input('Digite a porta desejada entre 10001 e 11000: '))
+    while (UDP_PORT_NO < 10001) or (UDP_PORT_NO > 11000):
+        print("A porta deve ser entre 10001 - 11000")
+        UDP_PORT_NO = int(raw_input('Digite a porta desejada entre 10001 e 11000: '))
+
+    pass
+#_____________________________________________________________
+
+def host(UDP_IP_ADDRESS, UDP_PORT_NO):
+
+    serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    serverSock.bind((UDP_IP_ADDRESS, UDP_PORT_NO))
+    print("Waiting... ")
+
+    while True:
+        data, addr = serverSock.recvfrom(1024)
+        data = data.decode()
+        print("Message: ", data)
+
+    pass
+
+#_____________________________________________________________
+
+def main():
+
+    config()
+    host(UDP_IP_ADDRESS, UDP_PORT_NO, MAX_MESSAGE)
+
+#_____________________________________________________________
+
+if __name__ == '__main__':
+    main()
